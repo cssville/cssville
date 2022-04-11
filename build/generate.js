13b5f8e4f4623e85b45aa32988e786d1e9469961
Evgeniy Kosjakov <42_@mail.ru>
2022-04-11 20:39:48 +0300

    small update

--- build/generate.js
+++ build/generate.js
@@ -3,5 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
 var cssville_1 = require("./cssville");
 var css = cssville_1.Cssville.css();
-fs.writeFile("build/cssville.css", css, function () { console.log("done."); });
+var path = process.argv[2];
+fs.writeFile(path, css, function () { console.log("done: " + path); });
 //# sourceMappingURL=generate.js.map
--- src/generate.ts
+++ src/generate.ts
@@ -2,4 +2,5 @@ import fs = require('fs');
 import { Cssville } from './cssville';
 
 var css = Cssville.css();
-fs.writeFile("build/cssville.css", css, () => { console.log("done."); });
+var path = process.argv[2];
+fs.writeFile(path, css, () => { console.log("done: " + path); });
