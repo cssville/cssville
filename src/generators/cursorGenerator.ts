c551acbc0ed496bc3016509bf71605d296321373
Evgeniy.Kosjakov <elv1s42k@gmail.com>
2023-02-17 19:47:21 +0200

    Update cursorGenerator.ts

--- src/generators/cursorGenerator.ts
+++ src/generators/cursorGenerator.ts
@@ -3,9 +3,20 @@ import { GeneratorBase } from "../GeneratorBase";
 
 export class CursorGenerator extends GeneratorBase {
     postfixValuesMap = new Map([
+        ["cell", ["cell"]],
+        ["help", ["help"]],
+        ["crosshair", ["crosshair"]],
+        ["text", ["text"]],
         ["wait", ["wait"]],
+        ["copy", ["copy"]],
+        ["move", ["move"]],
+        ["grab", ["grab"]],
+        ["grabbing", ["grabbing"]],
+        ["not-allowed", ["not-allowed"]],
         ["pointer", ["pointer"]],
         ["progress", ["progress"]],
+        ["zoom-in", ["zoom-in"]],
+        ["zoom-out", ["zoom-out"]],
         ["default", ["default"]]
     ]);
     cssData = [
